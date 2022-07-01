d4140b774d9bc7f11ea4ff66c38082882e26d71c
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2022-07-01 11:43:19 +0900

    add support for netstandard

---
+++ src/_Usings.cs
@@ -0,0 +1,7 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
+#if !NETSTANDARD2_0
+global using System.Collections.Immutable;
+#endif
